3ad01fa736a43c5f9cf4cbe2d682dbee16d2325b
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-05-16 16:53:07 +0200

    Init

---
+++ src/main/java/ch/bbw/m133_validation/M133ValidationApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.m133_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M133ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M133ValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/m133_validation/M133ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m133_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M133ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
