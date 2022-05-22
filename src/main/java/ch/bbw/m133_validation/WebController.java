0aba382599ce0c3796595c5f76f60dbeed6b745c
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-05-22 20:34:20 +0200

    All code from PDF

---
+++ src/main/java/ch/bbw/m133_validation/WebController.java
@@ -0,0 +1,32 @@
+package ch.bbw.m133_validation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.Valid;
+import java.util.logging.Logger;
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+    static Logger log = Logger.getLogger("WebController");
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("personForm", new PersonForm());
+        return "form";
+    }
+    @PostMapping("/")
+    public String checkPersonInfo(
+            @Valid PersonForm personForm,
+            BindingResult bindingResult // validation results
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
